5d01d310e612ca59aa6a54fe6c9e5602610a4c94
amimhayden22 <amimopreker22@gmail.com>
2024-08-12 11:39:52 +0700

    update code

--- resources/views/dashboard-layouts/sidebar.blade.php
+++ resources/views/dashboard-layouts/sidebar.blade.php
@@ -6,23 +6,23 @@
       </a>
       </div>
       <div class="sidebar-brand sidebar-brand-sm">
-        <a href="{{ route('dasbor') }}">
+        <a href="{{ route('dashboard') }}">
           <img src="{{ asset('assets/img/logo-sadasa-circle.png') }}" alt="Logo Circle" width="50" class="img-fluid">
       </a>
       </div>
       <ul class="sidebar-menu">
                 <li class="menu-header">Dasbor</li>
-                <li class="nav-item dropdown {{ (request()->is('dasbor')) ? 'active' : '' }}">
-                    <a href="{{ route('dasbor') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dasbor</span></a>
+                <li class="nav-item dropdown {{ (request()->is('dashboard')) ? 'active' : '' }}">
+                    <a href="{{ route('dashboard') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dasbor</span></a>
                 </li>
                 <li class="menu-header">Master Data</li>
-                <li class="{{ (request()->is('dasbor/parts*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('parts.index') }}"><i class="fas fa-handshake"></i> <span>Bagian</span></a></li>
-                <li class="{{ (request()->is('dasbor/positions*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('positions.index') }}"><i class="fas fa-map-pin"></i> <span>Jabatan</span></a></li>
-                <li class="{{ (request()->is('dasbor/employees*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('employees.index') }}"><i class="fas fa-users"></i> <span>Karyawan</span></a></li>
-                    <li class="{{ (request()->is('dasbor/users*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('users.index') }}"><i class="fas fa-user"></i> <span>Pengguna</span></a></li>
+                <li class="{{ (request()->is('dashboard/parts*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('parts.index') }}"><i class="fas fa-handshake"></i> <span>Bagian</span></a></li>
+                <li class="{{ (request()->is('dashboard/positions*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('positions.index') }}"><i class="fas fa-map-pin"></i> <span>Jabatan</span></a></li>
+                <li class="{{ (request()->is('dashboard/employees*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('employees.index') }}"><i class="fas fa-users"></i> <span>Karyawan</span></a></li>
+                    <li class="{{ (request()->is('dashboard/users*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('users.index') }}"><i class="fas fa-user"></i> <span>Pengguna</span></a></li>
 
                 <li class="menu-header">Perizinan</li>
-                <li class="{{ (request()->is('dasbor/work-permit*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('work-permit.index') }}"><i class="fas fa-calendar-alt"></i> <span>Pengajuan Izin Kerja</span></a></li>
+                <li class="{{ (request()->is('dashboard/work-permit*')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('work-permit.index') }}"><i class="fas fa-calendar-alt"></i> <span>Pengajuan Izin Kerja</span></a></li>
         </ul>
 
         <div class=" mb-4 p-3 hide-sidebar-mini">
